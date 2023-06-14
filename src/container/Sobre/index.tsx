f3df0c69092e40c3706a0a2783290fbf222b8fde
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-06-14 17:00:52 -0300

    "doing great on third commit"

--- src/container/Sobre/index.tsx
+++ src/container/Sobre/index.tsx
@@ -1,14 +1,20 @@
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
-    <Titulo>Sobre mim</Titulo>
-    <p>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident porro,
       recusandae voluptates, nesciunt tempore aperiam deserunt animi culpa quo
       accusantium eaque totam natus dolor illum praesentium maxime voluptatem
       error nisi?
-    </p>
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=kaynanka&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=kaynanka&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
--- src/container/sidebar/index.tsx
+++ src/container/sidebar/index.tsx
@@ -3,7 +3,7 @@ import Titulo from '../../components/Titulo'
 const Sidebar = () => (
   <aside>
     <img src="https://github.com/kaynanka.png" />
-    <Titulo>Kaynan Nascimento</Titulo>
+    <Titulo fontSize={20}>Kaynan Silva</Titulo>
   </aside>
 )
 
