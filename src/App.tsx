2a0fcdf018b0ee46ae63eb5680561bc095977803
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-06-15 11:28:22 -0300

    "lastest commit"

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
+import { ThemeProvider } from 'styled-components'
+import { useState } from 'react'
 import Projetos from './container/Projetos'
 import Sidebar from './container/sidebar'
 import Sobre from './container/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
+  const [esteUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!esteUsandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={esteUsandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
